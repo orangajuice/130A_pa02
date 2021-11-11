687834face5542e06205ff716442f9b38bc14741
JoejoeWang <49485325+JoejoeWang@users.noreply.github.com>
2021-11-11 15:28:58 -0800

    Add files via upload

---
+++ MM.h
@@ -0,0 +1,148 @@
+#ifndef MM_H
+#define MM_H
+
+#include "heap.h"
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+template <class T>
+class MM
+{
+	public:
+		MM(): size1(0), size2(0) {};
+		MM(int a, int b) :
+			size1(a), size2(b) {};
+		void insert(T a)
+		{
+			if(size1==0&&size2==0)//speical case when two heaps are empty
+			{
+				maximum = a;
+				minimum = a;
+				median = a;
+				max.insert(a);
+				size2++;
+			}
+			else
+			{
+				if(a > median)
+				{
+					min.insert(a);
+					size1++;
+				}
+				else
+				{
+					max.insert(a);
+					size2++;
+				}
+				//check balance
+				int def = size1 - size2;
+				if(def==2||def==-2) //rebalance
+				{
+					if(def > 1) //mean that min has too more
+					{
+						T newmax = min.extract_root();
+						max.insert(newmax);
+						median = max.get_root();
+					}
+					else if(def < 1) //mean that max has too more
+					{
+						T newmin = max.extract_root();
+						min.insert(newmin);
+						median = max.get_root();
+					}
+				}
+				else if(def==1)
+				{
+					median = min.get_root();
+					//check min
+					minimum = max.get_min();
+					//check max
+					maximum = min.get_max();
+				}
+				else if(def == -1)
+				{
+					median = max.get_root();
+					//check min
+					minimum = max.get_min();
+					//check max
+					maximum = min.get_max();
+				}
+			}
+		}
+		void remove(T a)
+		{
+			if(a > median)
+			{
+				min.remove(a);
+				size1--;
+			}
+			else
+			{
+				max.remove(a);
+				size2--
+			}
+			int def = size1 - size2;
+			if(def==2 || def==-2)
+			{
+				if(def > 1) //mean that min has too more
+				{
+					T newmax = min.extract_root();
+					max.insert(newmax);
+					median = max.get_root();
+				}
+				else if(def < -1)
+				{
+					T newmin = max.extract_root();
+					min.insert(newmin);
+					median = max.get_root();
+				}
+			}
+			else if(def==1)
+			{
+				median = min.get_root();
+				//check min
+				minimum = max.get_min();
+				//check max
+				maximum = min.get_max();
+			}
+			else if(def == -1)
+			{
+				median = max.get_root();
+				//check min
+				minimum = max.get_min();
+				//check max
+				maximum = min.get_max();
+			}
+		}
+		T get_median() {return median;}
+		T get_minimum() {return min;}
+		T get_maximum() {return max;}
+		int get_size(){return size1+size2;}
+		bool search(T a)
+		{
+			if(a > median)
+			{
+				return min.search(a);
+			}
+			else
+			{
+				return max.search(a);
+			}
+		}
+		int leftIndex(int i){return 2*i + 1;}
+		int rightIndex(int i){return 2*i + 2;}
+	private:
+		int size1; //size of min
+        int size2; //size of max
+        T minimum;
+        T maximum;
+        T median;
+		heap<T> min;
+		heap<T> max;
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include "heap.h"
+#include "MM.h"
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    string heaptype;
+    string insertword;
+    string removeword;
+    ifstream insertfile(argv[2]);
+    ifstream removefile(argv[3]);
+
+    if(heaptype == "heap")
+    {
+        int size = 0;
+        // std::vector<int> heap_array;
+        heap<int> mx(0, true);
+        heap<int> mi(0, false);
+        while(getline(insertfile,insertword))
+        {
+            size += 1;
+            mx.insert(stoi(insertword));
+            mi.insert(stoi(insertword));
+        }
+        insertfile.close();
+        while(getline(removefile,removeword))
+        {
+            size -= 1;
+            mx.remove(stoi(removeword));
+            mi.remove(stoi(removeword));
+        }
+        removefile.close();
+
+        cout << "Min Heap:" << endl;
+        cout << "Size = " << mi.get_size() << endl;
+        cout << "Min = " << mi.get_min() << endl;
+        cout << "Max = " << mi.get_max() << endl;
+        cout << "Max Heap:" << endl;
+        cout << "Size = " << mx.get_size() << endl;
+        cout << "Min = " << mx.get_min() << endl;
+        cout << "Max = " << mx.get_max() << endl;
+    }
+    else if(heaptype == "minmedianmax")
+    {
+        int size = 0;
+        std::vector<int> heap_array;
+        MM<int> MMHeap(0,0);
+        while(getline(insertfile,insertword))
+        {
+            size += 1;
+            MMHeap.insert(stoi(insertword));
+        }
+        insertfile.close();
+        while(getline(removefile,removeword))
+        {
+            size -= 1;
+            MMHeap.remove(stoi(removeword));
+        }
+        removefile.close();
+
+        cout << "MinMedianMaxSketch:" << endl;
+        cout << "Size = " << MMHeap.get_size() << endl;
+        cout << "Min = " << MMHeap.get_minimum() << endl;
+        cout << "Max = " << MMHeap.get_maximum() << endl;
+    }
+    return 0;
+}
